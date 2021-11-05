159014cf61d5698860992ed3596b9cd42ee17a6e
CityBear3 <68993998+CityBear3@users.noreply.github.com>
2021-11-05 19:19:01 +0900

    Add user select by email

--- src/main/kotlin/com/project/todo/repository/UserRepository.kt
+++ src/main/kotlin/com/project/todo/repository/UserRepository.kt
@@ -4,4 +4,5 @@ import com.project.todo.model.UserRecord
 
 interface UserRepository {
     fun create(userRecord: UserRecord)
+    fun selectByEmail(userRecord: UserRecord): UserRecord?
 }
--- src/main/kotlin/com/project/todo/repository/UserRepositoryImpl.kt
+++ src/main/kotlin/com/project/todo/repository/UserRepositoryImpl.kt
@@ -1,9 +1,13 @@
 package com.project.todo.repository
 
+import com.project.todo.mapper.UserDynamicSqlSupport.User.email
 import com.project.todo.mapper.UserMapper
 import com.project.todo.mapper.insert
+import com.project.todo.mapper.insertSelective
+import com.project.todo.mapper.selectOne
 import com.project.todo.model.UserRecord
 import com.project.todo.utils.factory.CreateSessionFactory
+import org.mybatis.dynamic.sql.util.kotlin.elements.isEqualTo
 import org.springframework.stereotype.Component
 
 @Component
@@ -17,5 +21,19 @@ class UserRepositoryImpl(private val createSessionFactory: CreateSessionFactory)
                 mapper.insert(userRecord)
                 session.commit()
             }
+    }
+
+    override fun selectByEmail(userRecord: UserRecord): UserRecord? {
+        val user: UserRecord?
+        createSessionFactory.newFactory()
+            .openSession()
+            .use {
+                session ->
+                val mapper = session.getMapper(UserMapper::class.java)
+                user = mapper.selectOne {
+                    where(email, isEqualTo(userRecord.email!!))
+                }
+            }
+        return user
     }
 }
