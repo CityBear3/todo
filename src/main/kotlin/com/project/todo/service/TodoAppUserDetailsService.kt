ebaf1b0cc463f52a1dcb2fbc2be9733f2a427834
CityBear3 <68993998+CityBear3@users.noreply.github.com>
2021-11-07 16:32:18 +0900

    Move TodoAppDetails

--- src/main/kotlin/com/project/todo/service/TodoAppUserDetailsService.kt
+++ src/main/kotlin/com/project/todo/service/TodoAppUserDetailsService.kt
@@ -1,5 +1,6 @@
 package com.project.todo.service
 
+import com.project.todo.entity.details.TodoAppUserDetails
 import com.project.todo.model.UserRecord
 import com.project.todo.model.enum.RoleType
 import org.springframework.security.core.GrantedAuthority
@@ -11,39 +12,5 @@ class TodoAppUserDetailsService(private val authenticationService: Authenticatio
     override fun loadUserByUsername(username: String?): UserDetails? {
         val user = authenticationService.findUser(username!!)
         return user?.let { TodoAppUserDetails(user) }
-    }
-}
-
-data class TodoAppUserDetails(val id: Int, val email: String, val pass: String, val roleType: RoleType) :
-    UserDetails {
-
-    constructor(user: UserRecord): this(user.id!!, user.email!!, user.password!!, user.role!!)
-
-    override fun getAuthorities(): MutableCollection<out GrantedAuthority> {
-        return AuthorityUtils.createAuthorityList(this.roleType.toString())
-    }
-
-    override fun isEnabled(): Boolean {
-        return true
-    }
-
-    override fun getUsername(): String {
-        return this.email
-    }
-
-    override fun isCredentialsNonExpired(): Boolean {
-        return true
-    }
-
-    override fun getPassword(): String {
-        return this.pass
-    }
-
-    override fun isAccountNonExpired(): Boolean {
-        return true
-    }
-
-    override fun isAccountNonLocked(): Boolean {
-        return true
     }
 }
