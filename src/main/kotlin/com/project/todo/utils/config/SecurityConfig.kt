57d00c3d8cc79344eef7079569cd451534dba3f0
CityBear3 <68993998+CityBear3@users.noreply.github.com>
2021-11-07 16:10:27 +0900

    Add auth config

--- src/main/kotlin/com/project/todo/utils/config/SecurityConfig.kt
+++ src/main/kotlin/com/project/todo/utils/config/SecurityConfig.kt
@@ -1,14 +1,46 @@
 package com.project.todo.utils.config
 
+import com.project.todo.api.handler.TodoAppAccessDeniedHandler
+import com.project.todo.api.handler.TodoAppAuthenticationEntryPoint
+import com.project.todo.api.handler.TodoAppAuthenticationFailureHandler
+import com.project.todo.api.handler.TodoAppAuthenticationSuccessHandler
+import com.project.todo.model.enum.RoleType
+import com.project.todo.service.AuthenticationService
+import com.project.todo.service.TodoAppUserDetailsService
 import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 
 @EnableWebSecurity
 @Configuration
-class SecurityConfig : WebSecurityConfigurerAdapter() {
-    override fun configure(http: HttpSecurity?) {
-        http?.authorizeRequests()?.mvcMatchers("/")?.permitAll()?.and()?.csrf()?.disable()
+class SecurityConfig(private val authenticationService: AuthenticationService) : WebSecurityConfigurerAdapter() {
+    override fun configure(http: HttpSecurity) {
+        http.authorizeRequests()
+            .mvcMatchers("/api/login").permitAll()
+            .mvcMatchers("/api/user/signup").permitAll()
+            .mvcMatchers("/api/admin/**").hasAuthority(RoleType.ADMIN.toString())
+            .anyRequest().authenticated()
+            .and()
+            .csrf().disable()
+            .formLogin()
+            .loginProcessingUrl("/api/login")
+            .usernameParameter("email")
+            .passwordParameter("password")
+            .successHandler(TodoAppAuthenticationSuccessHandler())
+            .failureHandler(TodoAppAuthenticationFailureHandler())
+            .and()
+            .exceptionHandling()
+            .authenticationEntryPoint(TodoAppAuthenticationEntryPoint())
+            .accessDeniedHandler(TodoAppAccessDeniedHandler())
+            .and()
+            .cors().disable()
+    }
+
+    override fun configure(auth: AuthenticationManagerBuilder) {
+        auth.userDetailsService(TodoAppUserDetailsService(authenticationService))
+            .passwordEncoder(BCryptPasswordEncoder())
     }
 }
