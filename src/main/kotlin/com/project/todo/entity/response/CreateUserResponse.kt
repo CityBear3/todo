8f6d7bcba32af6b85f64702291298c1d86851203
CityBear3 <68993998+CityBear3@users.noreply.github.com>
2021-11-07 16:00:53 +0900

    Refactor class name

---
+++ src/main/kotlin/com/project/todo/entity/response/CreateUserResponse.kt
@@ -0,0 +1,3 @@
+package com.project.todo.entity.response
+
+data class CreateUserResponse(val uid: Int, val message: String)
--- src/main/kotlin/com/project/todo/entity/response/UserCreateResponse.kt
+++
@@ -1,3 +0,0 @@
-package com.project.todo.entity.response
-
-data class UserCreateResponse(val uid: Int, val message: String)
