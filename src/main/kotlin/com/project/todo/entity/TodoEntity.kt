285df68e262ab35249aed4ad0a4a92825946a030
CityBear3 <68993998+CityBear3@users.noreply.github.com>
2021-11-07 16:05:26 +0900

    Feature update record

--- src/main/kotlin/com/project/todo/entity/TodoEntity.kt
+++ src/main/kotlin/com/project/todo/entity/TodoEntity.kt
@@ -1,23 +1,41 @@
 package com.project.todo.entity
 
 import com.project.todo.model.TodoRecord
+import java.util.*
 
 class TodoEntity(
-    private val title: String,
-    private val description: String,
-    private val done: Boolean,
-    private val userId: Int,
+    private val title: String? = null,
+    private val description: String? = null,
+    private val done: Boolean? = null,
+    private val userId: Int? = null,
 ) {
-    fun createRecode(): TodoRecord {
+    fun createRecord(): TodoRecord {
         return TodoRecord(
             title = title,
             description = description,
-            done = if (done) {
+            done = if (done!!) {
                 1.toByte()
             } else {
                 0.toByte()
             },
-            userId = userId
+            userId = userId,
+            createTime = Date(),
+            updateTime = Date(),
+        )
+    }
+
+    fun updateRecord(): TodoRecord {
+        return TodoRecord(
+            title = title,
+            description = description,
+            done = if (done!!) {
+                1.toByte()
+            } else {
+                0.toByte()
+            },
+            userId = userId,
+            createTime = null,
+            updateTime = Date(),
         )
     }
 }
