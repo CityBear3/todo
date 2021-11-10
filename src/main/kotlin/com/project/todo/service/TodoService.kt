68d1855cf70b7e825358d893aa32f2e7199e2d7f
CityBear3 <68993998+CityBear3@users.noreply.github.com>
2021-11-10 22:52:17 +0900

    Add user check

--- src/main/kotlin/com/project/todo/service/TodoService.kt
+++ src/main/kotlin/com/project/todo/service/TodoService.kt
@@ -1,16 +1,22 @@
 package com.project.todo.service
 
 import com.project.todo.entity.TodoEntity
+import com.project.todo.logic.UserCheckLogic
 import com.project.todo.repository.TodoRepository
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.stereotype.Service
 
 @Service
-class TodoService(private val todoRepository: TodoRepository) {
+class TodoService(private val todoRepository: TodoRepository, private val userCheckLogic: UserCheckLogic) {
     fun createTodo(todoEntity: TodoEntity): ResponseEntity<String> {
+        val record = todoEntity.createRecord()
+        if (!userCheckLogic.checkUser(record.userId!!)) {
+            return ResponseEntity("Auth Error", HttpStatus.FORBIDDEN)
+        }
+
         kotlin.runCatching {
-            todoRepository.create(todoEntity.createRecord())
+            todoRepository.create(record)
         }.fold(
             onSuccess = { return ResponseEntity("create todo is successful", HttpStatus.OK) },
             onFailure = { return ResponseEntity("create todo is failed", HttpStatus.INTERNAL_SERVER_ERROR) }
@@ -20,6 +26,10 @@ class TodoService(private val todoRepository: TodoRepository) {
     fun updateTodo(id: Int, todoEntity: TodoEntity): ResponseEntity<String> {
         val record = todoEntity.updateRecord()
         record.id = id
+        if (!userCheckLogic.checkUserByTodoId(record.id!!)) {
+            return ResponseEntity("Auth Error", HttpStatus.FORBIDDEN)
+        }
+
         kotlin.runCatching {
             todoRepository.update(record)
         }.fold(
