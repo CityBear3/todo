9622487f3bae8875dbc71c01e26093ac8355d725
CityBear3 <68993998+CityBear3@users.noreply.github.com>
2021-11-13 01:13:13 +0900

    Add exists check

--- src/main/kotlin/com/project/todo/application/service/UserApplicationService.kt
+++ src/main/kotlin/com/project/todo/application/service/UserApplicationService.kt
@@ -1,15 +1,19 @@
 package com.project.todo.application.service
 
-import com.project.todo.domain.entity.UserEntity
 import com.project.todo.api.response.CreateUserResponse
+import com.project.todo.domain.entity.UserEntity
 import com.project.todo.domain.model.UserRecord
 import com.project.todo.domain.repository.UserRepository
+import com.project.todo.domain.service.UserDomainService
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.stereotype.Service
 
 @Service
-class UserApplicationService(private val userRepository: UserRepository) {
+class UserApplicationService(
+    private val userRepository: UserRepository,
+    private val userDomainService: UserDomainService
+) {
     fun createUser(userEntity: UserEntity): ResponseEntity<CreateUserResponse> {
         var userRecord = UserRecord()
         if (!userEntity.checkEmail()) {
@@ -17,6 +21,16 @@ class UserApplicationService(private val userRepository: UserRepository) {
                 CreateUserResponse(
                     uid = -1,
                     message = "Please check your email address enough rule"
+                ),
+                HttpStatus.BAD_REQUEST
+            )
+        }
+
+        if (userDomainService.exists(userEntity)) {
+            return ResponseEntity(
+                CreateUserResponse(
+                    uid = -1,
+                    message = "This account has been already existed"
                 ),
                 HttpStatus.BAD_REQUEST
             )
@@ -36,13 +50,15 @@ class UserApplicationService(private val userRepository: UserRepository) {
             userRepository.create(userEntity.createRecode())
             userRecord = userRepository.selectByEmail(userEntity.createRecode())!!
         }.fold(
-            onSuccess = { return ResponseEntity(
-                CreateUserResponse(
-                    uid = userRecord.id!!,
-                    message = "User create was success"
-                ),
-                HttpStatus.OK
-            ) },
+            onSuccess = {
+                return ResponseEntity(
+                    CreateUserResponse(
+                        uid = userRecord.id!!,
+                        message = "User create was success"
+                    ),
+                    HttpStatus.OK
+                )
+            },
             onFailure = {
                 return ResponseEntity(
                     CreateUserResponse(
