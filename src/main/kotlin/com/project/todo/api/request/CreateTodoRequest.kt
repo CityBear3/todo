bbd9b507e5adf110f7d68330145fddad55c5afc2
CityBear3 <68993998+CityBear3@users.noreply.github.com>
2021-11-13 01:11:36 +0900

    Remove param

--- src/main/kotlin/com/project/todo/api/request/CreateTodoRequest.kt
+++ src/main/kotlin/com/project/todo/api/request/CreateTodoRequest.kt
@@ -1,3 +1,3 @@
 package com.project.todo.api.request
 
-data class CreateTodoRequest(val title: String, val description: String, val done: Boolean, val userId: Int)
+data class CreateTodoRequest(val title: String, val description: String)
