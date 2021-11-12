86e5faa1142da178b7735ff97d2ef8a932802267
CityBear3 <68993998+CityBear3@users.noreply.github.com>
2021-11-13 01:12:31 +0900

    Change done and userId

--- src/main/kotlin/com/project/todo/api/controller/TodoController.kt
+++ src/main/kotlin/com/project/todo/api/controller/TodoController.kt
@@ -1,23 +1,27 @@
 package com.project.todo.api.controller
 
-import com.project.todo.domain.entity.TodoEntity
 import com.project.todo.api.request.CreateTodoRequest
 import com.project.todo.api.request.UpdateTodoRequest
 import com.project.todo.application.service.TodoApplicationService
+import com.project.todo.domain.entity.TodoEntity
+import com.project.todo.utils.factory.UserInfoFactory
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("/api/todo")
-class TodoController(private val todoApplicationService: TodoApplicationService) {
+class TodoController(
+    private val todoApplicationService: TodoApplicationService,
+    private val userInfoFactory: UserInfoFactory
+) {
     @PostMapping("/new")
     fun createTodo(@RequestBody createTodoRequest: CreateTodoRequest): ResponseEntity<String> {
         return todoApplicationService.createTodo(
             TodoEntity(
                 title = createTodoRequest.title,
                 description = createTodoRequest.description,
-                done = createTodoRequest.done,
-                userId = createTodoRequest.userId
+                done = false,
+                userId = userInfoFactory.getUserInfo().id
             )
         )
     }
