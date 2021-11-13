34980f77601896bcc7b6c12688022c4fa3b8b0eb
CityBear3 <68993998+CityBear3@users.noreply.github.com>
2021-11-13 11:28:59 +0900

    Rename class

--- src/main/kotlin/com/project/todo/api/response/GetTodoResponse.kt
+++ src/main/kotlin/com/project/todo/api/response/GetTodoResponse.kt
@@ -2,4 +2,4 @@ package com.project.todo.api.response
 
 import com.project.todo.domain.model.TodoRecord
 
-data class CreateTodoResponse(val todos: List<TodoRecord>, val message: String)
+data class GetTodoResponse(val todos: List<TodoRecord>, val message: String)
