2ce03eb95c497ee5b9963b428998840d9fd33c9e
CityBear3 <68993998+CityBear3@users.noreply.github.com>
2021-11-13 11:26:25 +0900

    Refactor and Add getTodos

--- src/main/kotlin/com/project/todo/application/service/TodoApplicationService.kt
+++ src/main/kotlin/com/project/todo/application/service/TodoApplicationService.kt
@@ -1,14 +1,21 @@
 package com.project.todo.application.service
 
-import com.project.todo.domain.entity.TodoEntity
+import com.project.todo.api.response.GetTodoResponse
 import com.project.todo.application.logic.UserCheckLogic
+import com.project.todo.domain.entity.TodoEntity
+import com.project.todo.domain.model.TodoRecord
 import com.project.todo.domain.repository.TodoRepository
+import com.project.todo.utils.factory.UserInfoFactory
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.stereotype.Service
 
 @Service
-class TodoApplicationService(private val todoRepository: TodoRepository, private val userCheckLogic: UserCheckLogic) {
+class TodoApplicationService(
+    private val todoRepository: TodoRepository,
+    private val userCheckLogic: UserCheckLogic,
+    private val userInfoFactory: UserInfoFactory
+) {
     fun createTodo(todoEntity: TodoEntity): ResponseEntity<String> {
         val record = todoEntity.createRecord()
         if (!userCheckLogic.checkUser(record.userId!!)) {
@@ -34,6 +41,21 @@ class TodoApplicationService(private val todoRepository: TodoRepository, private
         }.fold(
             onSuccess = { return ResponseEntity("update todo is successful", HttpStatus.OK) },
             onFailure = { return ResponseEntity("update todo is failed", HttpStatus.INTERNAL_SERVER_ERROR) }
+        )
+    }
+
+    fun getTodos(): ResponseEntity<GetTodoResponse> {
+        var todos = listOf<TodoRecord>()
+        kotlin.runCatching {
+            todos = todoRepository.selectByUid(userInfoFactory.getUserInfo().id)
+        }.fold(
+            onSuccess = { return ResponseEntity(GetTodoResponse(message = "succeeded", todos = todos), HttpStatus.OK) },
+            onFailure = {
+                return ResponseEntity(
+                    GetTodoResponse(message = "failed", todos = null),
+                    HttpStatus.INTERNAL_SERVER_ERROR
+                )
+            }
         )
     }
 }
